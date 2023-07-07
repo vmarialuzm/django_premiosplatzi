642499b16e8419e51b667536f728ed87ea54aa39
vmarialuzm <vmarialuzm@gmail.com>
2023-07-06 22:09:46 -0500

    primeros test

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,33 @@
+import datetime
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+from .models import Question
 
-# Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_questions(self):
+        """was_published_recently() returns False for questions whose pub_date is in the future."""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text="¿Quién es el mejor Course Director de Platzi?",pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+    
+    def test_was_published_recently_with_old_questions(self):
+        """was_published_recently() returns False for questions whose pub_date is older than 1 day."""
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(question_text="¿Quién es el mejor Course Director de Platzi?",pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_questions(self):
+        """was_published_recently() returns True for questions whose pub_date is within the last day."""
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(question_text="¿Quién es el mejor Course Director de Platzi?",pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """ If no questions exist, an appropriate message is displayed. """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
